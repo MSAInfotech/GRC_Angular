48e2e4f21574e40a41441ae29e73a00f1a6d180a
Sufiyan Halai <halaisufiyan7@gmail.com>
2024-12-11 18:27:18 +0530

    Project details dashboard design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProjectCreateComponent } from './project-create/project-create.component';
-
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProjectCreateComponent],
+  imports: [ProjectDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
