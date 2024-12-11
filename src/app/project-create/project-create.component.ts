ae6475c681cea0962b753649cb02474da917a449
badal-gaadhe <badal.gadhe@msainfotech.in>
2024-12-11 18:22:28 +0530

    finalize design and made responsive ui

--- src/app/project-create/project-create.component.ts
+++ src/app/project-create/project-create.component.ts
@@ -1,12 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-project-create',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './project-create.component.html',
   styleUrl: './project-create.component.css'
 })
 export class ProjectCreateComponent {
+  currentTab: string = 'details';
+  isFinishTabEnabled: boolean = false;
+  progress: number = 0;
 
+  formData = {
+    projectName: '',
+    description: '',
+    standard: '',
+    mapToProject: '',
+    autoGenerate: '',
+  };
+
+  switchTab(tab: string) {
+    if (tab === 'finish' && !this.isFinishTabEnabled) {
+      return;
+    }
+    this.currentTab = tab;
+  }
+
+  updateProgressBar() {
+    const fields = Object.values(this.formData);
+    const filledFields = fields.filter(field => field.trim() !== '').length;
+    this.progress = (filledFields / fields.length) * 100;
+  }
+
+  onNext() {
+    if (this.progress === 100) {
+      this.isFinishTabEnabled = true;
+      this.switchTab('finish');
+    } else {
+      alert('Please fill out all required fields.');
+    }
+  }
+
+  onFinish() {
+    alert('Project Finished!');
+  }
 }
