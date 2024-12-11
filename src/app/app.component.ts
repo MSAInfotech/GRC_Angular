427f4d5a27536b96a2710c3929bd2955fa601aeb
Moin Juneja <moin.juneja@msainfotech.in>
2024-12-11 20:49:33 +0530

    Design Project cards Screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProjectCreateComponent } from './project-create/project-create.component';
+import { ProjectViewComponent } from './project-view/project-view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProjectCreateComponent],
+  imports: [RouterOutlet, ProjectCreateComponent, ProjectViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/project-view/project-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-view.component.html',
+  styleUrl: './project-view.component.css'
+})
+export class ProjectViewComponent {
+
+}
